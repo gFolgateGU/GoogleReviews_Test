195630c4c2e57ec0f8cc00838342f5734a0f8e68
gFolgateGU <gfolgate@outlook.com>
2017-11-10 15:55:49 -0500

    Initial Project

---
+++ GoogleReviews_TestApp/GoogleReviews_TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+//using Google.Apis.
+
+namespace GoogleReviews_TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            
+            Console.Read();
+        }
+    }
+}
+ 
